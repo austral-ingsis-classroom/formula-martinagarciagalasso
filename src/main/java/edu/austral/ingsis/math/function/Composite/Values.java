b9b87ed58978e5f030693751f796e9ba00f76154
martinagarciagalasso <mgarciagalasso@mail.austral.edu.ar>
2024-06-24 18:55:19 -0300

    values and variables

--- src/main/java/edu/austral/ingsis/math/function/Composite/Values.java
+++ src/main/java/edu/austral/ingsis/math/function/Composite/Values.java
@@ -1,4 +1,30 @@
 package edu.austral.ingsis.math.function.Composite;
 
-public class Values {
+import java.util.List;
+import java.util.Map;
+
+public class Values implements Function{
+    private final double number;
+
+    public Values(double number) {
+        this.number = number;
+    }
+
+    @Override
+    public double evaluate(Map<String, Double> variables) {
+        return number;
+    }
+
+    @Override
+    public List<String> getMyVariables() {
+        return List.of();
+    }
+
+    @Override
+    public String print() {
+        if (number % 1 == 0) {
+            return Integer.toString((int) number);
+        }
+        return Double.toString(number);
+    }
 }
--- src/main/java/edu/austral/ingsis/math/function/Composite/Variables.java
+++ src/main/java/edu/austral/ingsis/math/function/Composite/Variables.java
@@ -1,4 +1,32 @@
 package edu.austral.ingsis.math.function.Composite;
 
-public class Variables {
+import java.util.List;
+import java.util.Map;
+
+public class Variables implements Function{
+    private String nombre;
+    private Double valor;
+
+    public Variables(String nombre, Double valor) {
+        this.nombre = nombre;
+        this.valor = valor;
+    }
+    public void setValue(Double valor) {
+        this.valor = valor;
+    }
+
+    @Override
+    public double evaluate(Map<String, Double> variables) {
+        return valor;
+    }
+
+    @Override
+    public List<String> getMyVariables() {
+        return List.of(nombre);
+    }
+
+    @Override
+    public String print() {
+        return nombre;
+    }
 }
