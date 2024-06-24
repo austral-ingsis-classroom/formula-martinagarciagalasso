ff8480ca719f2193431fcbd15d3bac0a82267935
martinagarciagalasso <mgarciagalasso@mail.austral.edu.ar>
2024-06-24 18:29:05 -0300

    pow

--- src/main/java/edu/austral/ingsis/math/function/Composite/Opperands/Power.java
+++ src/main/java/edu/austral/ingsis/math/function/Composite/Opperands/Power.java
@@ -2,22 +2,36 @@ package edu.austral.ingsis.math.function.Composite.Opperands;
 
 import edu.austral.ingsis.math.function.Composite.Function;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class Power implements Function {
+    private final Function leftFunction;
+    private final Function rightFunction;
+
+    public Power(Function leftFunction, Function rightFunction) {
+        this.leftFunction = leftFunction;
+        this.rightFunction = rightFunction;
+
+    }
+
     @Override
     public double evaluate(Map<String, Double> variables) {
-        return 0;
+        return Math.pow(leftFunction.evaluate(variables), rightFunction.evaluate(variables));
     }
 
     @Override
     public List<String> getMyVariables() {
-        return null;
+        List<String> leftVariables = leftFunction.getMyVariables();
+        List<String> rightVariables = rightFunction.getMyVariables();
+        List<String> combinedVariables = new ArrayList<>(leftVariables);
+        combinedVariables.addAll(rightVariables);
+        return combinedVariables;
     }
 
     @Override
     public String print() {
-        return null;
+        return "(" + leftFunction.print() + " ^ " + rightFunction.print() + ")";
     }
 }
