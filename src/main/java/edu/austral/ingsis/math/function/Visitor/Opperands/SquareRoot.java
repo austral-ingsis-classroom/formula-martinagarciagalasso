aeedafc995ed022ed62ffd99c229ddf53e27a812
martinagarciagalasso <mgarciagalasso@mail.austral.edu.ar>
2024-06-24 18:36:19 -0300

    raiz

--- src/main/java/edu/austral/ingsis/math/function/Visitor/Opperands/SquareRoot.java
+++ src/main/java/edu/austral/ingsis/math/function/Visitor/Opperands/SquareRoot.java
@@ -1,4 +1,29 @@
 package edu.austral.ingsis.math.function.Visitor.Opperands;
 
-public class SquareRoot {
+import edu.austral.ingsis.math.function.Composite.Function;
+
+import java.util.List;
+import java.util.Map;
+
+public class SquareRoot implements Function {
+    private final Function values;
+
+    public SquareRoot(Function values) {
+        this.values = values;
+    }
+
+    @Override
+    public double evaluate(Map<String, Double> variables) {
+        return Math.sqrt(values.evaluate(variables));
+    }
+
+    @Override
+    public List<String> getMyVariables() {
+        return values.getMyVariables();
+    }
+
+    @Override
+    public String print() {
+        return "sqrt(" + values.print() + ")";
+    }
 }
--- src/main/java/edu/austral/ingsis/math/function/Visitor/Opperands/Substraction.java
+++ src/main/java/edu/austral/ingsis/math/function/Visitor/Opperands/Substraction.java
@@ -1,4 +1,24 @@
 package edu.austral.ingsis.math.function.Visitor.Opperands;
 
-public class Substraction {
+import edu.austral.ingsis.math.function.Composite.Function;
+
+import java.util.List;
+import java.util.Map;
+
+public class Substraction implements Function {
+
+    @Override
+    public double evaluate(Map<String, Double> variables) {
+        return 0;
+    }
+
+    @Override
+    public List<String> getMyVariables() {
+        return null;
+    }
+
+    @Override
+    public String print() {
+        return null;
+    }
 }
